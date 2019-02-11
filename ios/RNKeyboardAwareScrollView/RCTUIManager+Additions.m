09a3d2c4d7ba18be6ddc47a647aa1817a21116a9
Pinar Olguc <pinarolguc@gmail.com>
2019-02-11 10:59:10 +0300

    Improve caret position detection
    We are putting a delay until caret position is available. It is probably becoming available
    after the height is adjusted and caret is viewable.

--- ios/RNKeyboardAwareScrollView/RCTUIManager+Additions.m
+++ ios/RNKeyboardAwareScrollView/RCTUIManager+Additions.m
@@ -1,6 +1,8 @@
 #import "RCTUIManager+Additions.h"
 #import <UIKit/UIKit.h>
 
+const CGFloat RNTDetectCaretPositionTrialCount = 20;
+
 @implementation RCTUIManager (Additions)
 
 RCT_EXPORT_METHOD(measureSelectionInWindow:(nonnull NSNumber *)reactTag callback:(RCTResponseSenderBlock)callback)
@@ -12,34 +14,75 @@ RCT_EXPORT_METHOD(measureSelectionInWindow:(nonnull NSNumber *)reactTag callback
             callback(@[@"View doesn't exist"]);
             return;
         }
-        CGRect windowFrame = [newResponder.window convertRect:newResponder.frame fromView:newResponder.superview];
         if ( [newResponder conformsToProtocol:@protocol(UITextInput)] ) {
             id<UITextInput> textInput = (id<UITextInput>)newResponder;
             UITextPosition *endPosition = textInput.selectedTextRange.end;
             if ( endPosition ) {
-                CGRect selectionEndRect = [textInput caretRectForPosition:endPosition];
-                CGFloat textInputBottomTextInset = 0;
-                if ( [textInput isKindOfClass:[UITextView class]] ) {
-                    textInputBottomTextInset = ((UITextView *)textInput).textContainerInset.bottom;
-                }
-                callback(@[[NSNull null],
-                           @(windowFrame.origin.x),  //text input x
-                           @(windowFrame.origin.y),  //text input y
-                           @(windowFrame.size.width),  //text input width
-                           @(windowFrame.size.height),  //text input height
-                           @(windowFrame.origin.x + selectionEndRect.origin.x),  //caret x
-                           @(windowFrame.origin.y + selectionEndRect.origin.y),  //caret y
-                           @(selectionEndRect.origin.x),  //caret relative x
-                           @(selectionEndRect.origin.y),  //caret relative y
-                           @(selectionEndRect.size.width),  //caret width
-                           @(selectionEndRect.size.height),  //caret height
-                           @(textInputBottomTextInset)  // text input bottom text inset
-                           ]);
+                [self rnt_caretRectIn:textInput trialCount:RNTDetectCaretPositionTrialCount completion:^(NSError *error, CGRect selectionEndRect) {
+                    if (error) {
+                        RCTLogWarn(@"measureSelectionInWindow cannot find the caret position in view with tag #%@", reactTag);
+                        callback(@[@"Caret position could not be determined"]);
+                        return;
+                    }
+                    CGRect windowFrame = [newResponder.window convertRect:newResponder.frame fromView:newResponder.superview];
+                    CGFloat textViewHeight = [self rnt_contentHeightIn:textInput defaultHeight:windowFrame.size.height];
+                    CGFloat textInputBottomTextInset = 0;
+                    if ( [textInput isKindOfClass:[UITextView class]] ) {
+                        textInputBottomTextInset = ((UITextView *)textInput).textContainerInset.bottom;
+                    }
+                    callback(@[[NSNull null],
+                               @(windowFrame.origin.x),  //text input x
+                               @(windowFrame.origin.y),  //text input y
+                               @(windowFrame.size.width),  //text input width
+                               @(textViewHeight),  //text input height
+                               @(windowFrame.origin.x + selectionEndRect.origin.x),  //caret x
+                               @(windowFrame.origin.y + selectionEndRect.origin.y),  //caret y
+                               @(selectionEndRect.origin.x),  //caret relative x
+                               @(selectionEndRect.origin.y),  //caret relative y
+                               @(selectionEndRect.size.width),  //caret width
+                               @(selectionEndRect.size.height),  //caret height
+                               @(textInputBottomTextInset)  // text input bottom text inset
+                               ]);
+                    return;
+                    
+                }];
                 return;
             }
         }
         callback(@[@"Text selection not available"]);
     }];
+}
+
+- (CGFloat)rnt_contentHeightIn:(id<UITextInput>)textInput defaultHeight:(CGFloat)defaultHeight {
+    if ([textInput isKindOfClass:[UITextView class]]) {
+        UITextView *textView = (UITextView *)textInput;
+        // This is a workaround to get the result height of the textview after its content is changed
+        // sometimes at this point the height of the textview is calculated wrongly and
+        // calling layoutIfNeeded doesn't help with that so we call sizeThatFits
+        return [textView sizeThatFits:textView.frame.size].height;
+    }
+    return defaultHeight;
+}
+
+- (void)rnt_caretRectIn:(id<UITextInput>)textInput trialCount:(NSInteger)trialCount completion:(void (^)(NSError *error, CGRect rect))completion {
+    if ( trialCount == 0 ) {
+        //We tried but couldn't find, return an error
+        completion([NSError new], CGRectZero);
+        return;
+    }
+    UITextPosition *endPosition = textInput.selectedTextRange.end;
+    CGRect selectionEndRect = [textInput caretRectForPosition:endPosition];
+    if ((CGRectGetMinY(selectionEndRect) < 0) ||
+        (INFINITY == CGRectGetMinY(selectionEndRect))) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)),
+                       dispatch_get_main_queue(),
+                       ^{
+                           // Recall
+                           [self rnt_caretRectIn:textInput trialCount:trialCount - 1 completion: completion];
+                       });
+        return;
+    }
+    completion(nil, selectionEndRect);
 }
 
 @end
